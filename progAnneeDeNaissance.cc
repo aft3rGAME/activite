97ca97d2284e32eb1c5c0efc30563b7d75c070d2
aft3rGAME <aft3rGAME@hotmail.fr>
2014-09-29 21:20:31 +0200

    ajout du programme de l'annee de naissance.

---
+++ progAnneeDeNaissance.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std
+
+int main()
+{
+	const int annee(2014);
+	int age(0), naissance(0);
+
+	cout << "Quel age a tu?" << endl;
+	cin >> age ;
+	
+	naissance=annnee-age;
+
+	cout << "tu es donc nes en " << naissance << endl;
+
+	return 0;
+
+}
